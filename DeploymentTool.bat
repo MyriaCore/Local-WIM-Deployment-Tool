ff7c44d4df92a30a385dec50237fd798f02e0aa0
Exerosis <theexerosisgaming@gmail.com>
2018-01-06 06:26:39 -0500

    ~Finished Build & Capture ~Did a lot of image related testing. ~Fixed a bunch of bugs.

--- DeploymentTool.bat
+++ DeploymentTool.bat
@@ -41,7 +41,14 @@ setlocal enabledelayedexpansion
 
 ::Print Welcome Instructions
 echo Welcome to the WespenJagerWindows Deployment Tool.
-echo "Type 'help' for help, or 'help [command]' for help with a specific command."
+echo Type 'help' for help, or 'help [command]' for help with a specific command.
+
+if not exist "Builds" (
+	mkdir Builds
+)
+if not exist "Images" (
+	mkdir Images
+)
 goto Main
 
 :: setup workspace
@@ -55,10 +62,11 @@ REM goto Index
 :Main
 	set /p command="Deployment Tool> "
 	cls
+
 	set /a count=0
 	for %%i in (%command%) do (
 		set "command!count!=%%i"
-		set /a count+=1
+        set /a count+=1
 	)
 
 	::Handle commands
@@ -68,95 +76,71 @@ REM goto Index
 		)
 		goto Help%command1%
 	)
-	:: If the user's command doesn't match up with any existing commands, throw an error.
 	if /I "%command0%"=="Capture" (
-	  goto Capture
-  )
-  if /I "%command0%"=="Build" (
-    goto Build
-  )
-  if /I "%command0%"=="Export" (
-    goto Export
-  )
+		goto Capture
+	)
+	if /I "%command0%"=="Build" (
+		goto Build
+	)
+	if /I "%command0%"=="Export" (
+		goto Export
+	)
 	if /I "%command0%"=="Exit" (
-  	exit /b
-  )
-  echo Unknown command. Type 'help' for help.
+		exit /b
+	)
+
+	:: If the user's command doesn't match up with any existing commands, throw an error.
+	echo Unknown command. Type 'help' for help.
 goto Main
 
-:: Tested! It Works!
 :Capture
-	::Set high-performance power scheme to speed deployment
-	if /I "%command3%"=="/h" (call powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c)
-	:: If Images Folder doesn't exist, create it and continue.
-	mkdir "%~dp0Images"
-	dism /Capture-Image /ImageFile:"%~dp0Images\%command2%.wim" /CaptureDir:%command1%\ /Name:"%command2%" /Compress:max /CheckIntegrity
-
+	if %count% geq 3 (
+		if /I "%command4%"=="/fd" (
+			set /p description=<%command3%
+		) else (
+			set description=%command3%
+		)
+		echo dism /Capture-Image /ImageFile:Images\%command2%.wim /CaptureDir:%command1% /Name:%command2% /Description:!description! /Compress:max /CheckIntegrity
+	) else (
+		echo Not enough arguments! Type 'help capture' for help.
+	)
 goto Main
 
-:: Need to test!
 :Build
-	:: Debugging Notes =======
-	:: Batch file Can't execute the final for loop.
-	:: When I isolate the dism command and run that by itself, it throws DISM Error 87.
-	:: This error seems to be for if you make a syntax error in the dism command
-	:: I've searched far and wide, but I cannot find where I have it wrong.
-	:: To test, I've found the dism logs and pulled out what it ended up writing as a dism command.
-	:: Here is what it executed as:
-	:: dism  /Export-Image /SourceImageFile:H:\Images\testimage.wim /DestinationImageFile:H:\Builds\testbuild.esd /Compress:recovery /CheckIntegrity
-	:: H: is the drive I have the DeploymentTool.bat on. an Images and Builds folder exists at its root.
-	:: I've fiddled with this specific command in isolation, but it seems that we simply are getting the /Export-Image command wrong somehow.
-	:: I'm leaving a link to my DISM logs if you want to look through them.
-	:: DISM Log: https://goo.gl/xU2dHc (It's a gist)
-
-	:: Looks for /h as last command
-	if /I "%command4%"=="/h" (call powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c)
-
-	:: If Images Folder doesn't exist, throw an error
-	:: If Builds Folder doesn't exist, create it and continue
-	mkdir %~dp0Builds
-
-	:: Loops through ImageNames and exports them into a Build with filename BuildName
-	:: Can't get here and instead exits with the message "Syntax is incorrect" or something like that
-    dism /Export-Image /SourceImageFile:"%~dp0Images\%%i.wim" /DestinationImageFile:"%~dp0Builds\%command1%.esd" /Compress:recovery /CheckIntegrity
-
+	if %count% geq 2 (
+		if "%command2%"=="" (
+			for %%f in (Images\*.wim) do (
+				echo dism /Export-Image /SourceImageFile:Images\%%f /SourceName:%%~nf /DestinationImageFile:Builds\%command1%.esd /Compress:recovery /CheckIntegrity
+			)
+		) else (
+			for %%i in (%command%) do (
+        		if not %%i==%command0% (
+        			if not %%i==%command1% (
+        				echo dism /Export-Image /SourceImageFile:Images\%%i.wim /SourceName:%%i /DestinationImageFile:Builds\%command1%.esd /Compress:recovery /CheckIntegrity
+        			)
+        		)
+        	)
+		)
+	)
+	)else (
+		echo Not enough arguments! Type 'help capture' for help.
+	)
 goto Main
 
 :: Need to test!
 :Export
-	::Set high-performance power scheme to speed deployment
-	if /I "%command3%"=="/h" (call powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c)
-
 	echo "Beginning to export to drive: %command1%"
-	copy "%~dp0Builds\%command2%.esd" "%command1%\sources\install.esd" /Y /B
+	copy "Builds\%command2%.esd" "%command1~0,1%\sources\install.esd" /Y /B
     echo "Export complete, install.esd was replaced!"
 goto Main
-
-:: Commented out Index Label
-REM :Index
-REM 	set imagecount=0
-REM 	set buildcount=0
-REM 	mkdir %workspaceroot%
-REM
-REM 	for /R "%workspaceroot%\" %%g in (*.wim) do (
-REM      set imagenamearray[%imagecount%]=%%g
-REM      set imagecount+=1
-REM 	)
-REM
-REM  for /R "%workspaceroot%\" %%g in (*.esd) do (
-REM      set buildnamearray[%buildcount%]=%%g
-REM      set buildcount+=1
-REM  )
-REM goto Main
 
 ::Setup Help
 :Help
 	echo ----------------------------------
 	echo Availible Commands:
-	echo capture [volume] [name] /h   ^| Creates an image.
-	echo build [buildName] [names]... /h ^| Builds an ESD.
-	echo export [volume] [buildName] /h  ^| Exports an ESD to bootable media.
-	REM echo index                         ^| Re-Indexes the workspace
+	echo capture [directory] [name] [description] /fd  ^| Creates an image.
+	echo build [buildName] [images]... ^| Builds an ESD.
+	echo export [volume] [buildName]  ^| Exports an ESD to bootable media.
 	echo exit                           ^| Exits the Deployment Tool.
 	echo help [command]               ^| Shows detailed help.
 	echo ----------------------------------
@@ -164,21 +148,22 @@ goto Main
 
 :HelpCapture
 	echo ----------------------------------
-	echo Command: capture [volume] [name] /h
-	echo Usage: capture C: MyImage /h
-	echo volume: The volume letter to caputure an image of.
-	echo name: The name to give the captured image.
-	echo /h: Use high performance mode to create the image faster.
+	echo Command: capture [directory] [name] [description] /fd
+	echo Usage: capture C:\ MyImage MyDescription
+	echo Usage: capture C:\ MyImage C:\description.txt /fd
+	echo directory: The directory to caputure an image of.
+	echo name: The name to give the image.
+	echo description: The description to give to the image.
+	echo /fd: Indicates that [description] is a file path.
 	echo ----------------------------------
 goto Main
 
 :HelpBuild
 	echo ----------------------------------
-	echo build [buildName] [names]... /h
+	echo build [buildName] [images]...
 	echo Usage: build MyBuild MyFirstImage MySecondImage
 	echo buildName: The name of the build.
-	echo names...: A list of image names to add to the build, or blank to build all.
-	echo /h: Use high performance mode to build the ESD faster.
+	echo images...: A list of image names to add to the build, or blank to build all.
 	echo ----------------------------------
 goto Main
 
@@ -188,6 +173,5 @@ goto Main
 	echo Usage: export F: MyBuild /h
 	echo volume: The volume containing bootable media to export to.
 	echo buildName: The name of the build to export.
-	echo /h: Use high performance mode to export the build faster
 	echo ----------------------------------
 goto Main
